21a3398bcea56597fbb07f102fc8720340ed25a1
Heun11 <pacificko787@gmail.com>
2024-01-01 16:23:42 +0100

    obrazky

--- source/main.c
+++ source/main.c
@@ -8,7 +8,8 @@ int SCREEN_HEIGHT;
 int TS;
 
 typedef struct{
-  float x, y, d, s, a, r;
+  float x, y, d, s, a, r, ra;
+  int t;
 }asteroid;
 
 int circleCollision(int x1, int y1, int r1, int x2, int y2, int r2)
@@ -36,10 +37,12 @@ int main(void)
   float dt = 1;
 
   TS = SCREEN_HEIGHT/20;
+  Texture2D tileset = LoadTexture("resources/tileset.png");
 
   float a = 0;
+  float pa = 0;
   Rectangle p = {(float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2, TS, TS*2};
-  Rectangle d = {(float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2, TS*2, (float)TS/2};
+  Rectangle d = {(float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2, TS, TS};
   float defDist = TS*2.5;
   float distance = defDist; 
   int onGround = 0;
@@ -54,7 +57,7 @@ int main(void)
   int asteroidsLen = 2;
   asteroid asteroids[100];
   for(int i=0;i<100;i++){
-    asteroids[i] = (asteroid){-1,-1,-1,-1,-1,-1};
+    asteroids[i] = (asteroid){-1,-1,-1,-1,-1,-1,-1,-1};
   }
 
   while (!WindowShouldClose()){
@@ -70,7 +73,7 @@ int main(void)
           health--;
         }
 
-        if(showDefense && circleCollision(asteroids[i].x, asteroids[i].y, asteroids[i].r, d.x, d.y, d.height)){
+        if(showDefense && circleCollision(asteroids[i].x, asteroids[i].y, asteroids[i].r, d.x, d.y, d.height/2)){
           asteroids[i].d = -1;
           score++;
           if(asteroidsLen<100){
@@ -83,15 +86,20 @@ int main(void)
           asteroids[i].s = (float)GetRandomValue(1, 30)/10;
           asteroids[i].a = (float)GetRandomValue(0, 360);
           asteroids[i].r = (float)GetRandomValue(TS*0.3, TS);
+          asteroids[i].t = GetRandomValue(0, 3);
+          asteroids[i].ra = 0;
         }
-        
+       
+        asteroids[i].ra += asteroids[i].s/2;
         asteroids[i].d -= asteroids[i].s;
         asteroids[i].x = (float)SCREEN_WIDTH/2 + (sin(asteroids[i].a/180*PI)*asteroids[i].d);
         asteroids[i].y = (float)SCREEN_HEIGHT/2 - (cos(asteroids[i].a/180*PI)*asteroids[i].d);
-        DrawCircle(asteroids[i].x, asteroids[i].y, asteroids[i].r, YELLOW);
+        // DrawCircle(asteroids[i].x, asteroids[i].y, asteroids[i].r, YELLOW);
+        DrawTexturePro(tileset, (Rectangle){96+32*asteroids[i].t,0,32,32}, (Rectangle){asteroids[i].x, asteroids[i].y, asteroids[i].r*2, asteroids[i].r*2}, (Vector2){(float)asteroids[i].r, (float)asteroids[i].r}, asteroids[i].ra, RAYWHITE);
       }
 
       yvel += gravity;
+      pa += 0.2f;
 
       distance+=yvel;
       if(distance<=defDist){
@@ -110,12 +118,18 @@ int main(void)
         health = 0;
       }
 
-      DrawCircle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, defDist, RED);
+      // DrawCircle(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, defDist, RED);
+      DrawTexturePro(tileset, (Rectangle){64,64,32*6,32*6}, (Rectangle){(float)SCREEN_WIDTH/2, (float)SCREEN_HEIGHT/2, defDist*2, defDist*2}, (Vector2){(float)defDist, (float)defDist}, pa, RAYWHITE);
       DrawText(TextFormat("Health: %d", health), (float)SCREEN_WIDTH/2-MeasureTextEx(GetFontDefault(), TextFormat("Health: %d", health), (float)TS/1.5, 1).x/2, SCREEN_HEIGHT/2, TS/1.5, BLACK);
-      DrawRectanglePro(p, (Vector2){p.width/2,p.height}, a, GREEN);
       DrawText(TextFormat("Score: %d", score), 50, 50, TS/2, WHITE);
       if(showDefense){
-        DrawCircle(d.x, d.y, d.height, BLUE);
+        DrawTexturePro(tileset, (Rectangle){32,0,32,64}, p, (Vector2){p.width/2,p.height}, a, RAYWHITE);
+        // DrawCircle(d.x, d.y, d.height/2, BLUE);
+        DrawTexturePro(tileset, (Rectangle){64,0,32,32}, d, (Vector2){d.width/2,d.height/2}, a, RAYWHITE);
+      }
+      else{
+        // DrawRectanglePro(p, (Vector2){p.width/2,p.height}, a, GREEN);
+        DrawTexturePro(tileset, (Rectangle){0,0,32,64}, p, (Vector2){p.width/2,p.height}, a, RAYWHITE);
       }
       DrawText("Move with AD and SPACE + W is schield, save planet, good luck!", TS*4, 50, TS/2, GREEN);
 
